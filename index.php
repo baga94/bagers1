5f694f5163c587fe39a3e350761e41f8f5f927c9
baga94 <37371002+baga94@users.noreply.github.com>
2018-03-14 15:39:18 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,349 @@
+<?php get_header()?>
+	<!-- Start services section -->
+	<section id="services">
+		<div class="container">
+			<header>
+				<h2 class="wow fadeInDown">Наши услуги</h2>
+				<p class="wow fadeInUp" data-wow-delay="0.1s">Мы предоставляем широкий спектр услуг в области
+					косметологии и массажа
+				</p>
+			</header>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="service-item text-right wow fadeInUp">
+						<div class="service-text">
+							<h3>Design</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								empor incididunt ut labore et dolore magna aliqua.
+							</p>
+						</div>
+						<div class="service-icon">
+							<i class="fa fa-diamond"></i>
+						</div>
+					</div>
+					<div class="service-item text-right wow fadeInUp" data-wow-delay="0.1s">
+						<div class="service-text">
+							<h3>Coding</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								empor incididunt ut labore et dolore magna aliqua.
+							</p>
+						</div>
+						<div class="service-icon">
+							<i class="fa fa-code"></i>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center">
+					<img src=<?php bloginfo('template_url')?>/img/phone.png alt="Phone" />
+				</div>
+				<div class="col-md-4">
+					<div class="service-item text-left wow fadeInUp">
+						<div class="service-text">
+							<h3>Photography</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								empor incididunt ut labore et dolore magna aliqua. 
+							</p>
+						</div>
+						<div class="service-icon">
+							<i class="fa fa-camera"></i>
+						</div>
+					</div>
+					<div class="service-item text-left wow fadeInUp" data-wow-delay="0.1s">
+						<div class="service-text">
+							<h3>Apps</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								empor incididunt ut labore et dolore magna aliqua.
+							</p>
+						</div>
+						<div class="service-icon">
+							<i class="fa fa-cubes"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="video-wrapper clearfix">
+		<div class="col-md-offset-1 col-md-10 video">
+			<a href="https://www.youtube.com/watch?v=CnYtWWDor2s" id="popup-youtube" class="circled fa fa-play" ></a>
+			<h3>Play video and be amazed</h3>
+			<p>Than lovely frames</p>
+		</div>
+	</div>
+</section>
+<!-- End services section -->
+<!-- Start portfolio section -->
+<section id="portfolio">
+	<header>
+		<h2 class="wow fadeInDown">What we done so far</h2>
+		<p class="wow fadeInUp" data-wow-delay="0.1s">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat.
+		</p>
+	</header>
+	<div class="content" id="filter-controls">
+		<nav class="centered-pills clearfix">
+			<ul class="nav nav-pills" >
+				<li><a href="javascript:void(0);" class="filter active" data-filter="all">All</a></li>
+				<li><a href="javascript:void(0);" class="filter" data-filter=".mockups">Mockups</a></li>
+				<li><a href="javascript:void(0);" class="filter" data-filter=".icons">Icons</a></li>
+				<li><a href="javascript:void(0);" class="filter" data-filter=".ui-kits">UI Kits</a></li>
+				<li><a href="javascript:void(0);" class="filter" data-filter=".templates">Templates</a></li>
+			</ul>
+		</nav>
+		<div class="projects row">
+			<figure class="mix all portfolio-item icons col-md-3 col-sm-6" >
+				<img class="img-responsive" src=http://kosmetologist.com/wp-content/themes/Kosmo/img/portfolio/adena.png alt="Adena icons pack" />
+				<figcaption class="mask">
+					<h3>Adena icons pack</h3>
+					<span>Icons</span>
+				</figcaption>
+			</figure>
+			<figure class="mix portfolio-item ui-kits col-md-3  col-sm-6">
+				<img class="img-responsive" src=http://kosmetologist.com/wp-content/themes/Kosmo/img/portfolio/atom.png alt="Atom UI Kit" />
+				<figcaption class="mask">
+					<h3>Atom UI Kit</h3>
+					<span>UI Kits</span>
+				</figcaption>
+			</figure>
+			<figure class="mix portfolio-item icons col-md-3  col-sm-6">
+				<img class="img-responsive" src=http://kosmetologist.com/wp-content/themes/Kosmo/img/portfolio/flat.png alt="60 flat icons" />
+				<figcaption class="mask">
+					<h3>60 flat icons</h3>
+					<span>Icons</span>
+				</figcaption>
+			</figure>
+			<figure class="mix portfolio-item mockups col-md-3  col-sm-6">
+				<img class="img-responsive" src=http://kosmetologist.com/wp-content/themes/Kosmo/img/portfolio/ipad.jpg alt="iPad mockup" />
+				<figcaption class="mask">
+					<h3>iPad Mockup</h3>
+					<span>Mockups</span>
+				</figcaption>
+			</figure>
+			<figure class="mix portfolio-item templates col-md-3  col-sm-6">
+				<img class="img-responsive" src=http://kosmetologist.com/wp-content/themes/Kosmo/img/portfolio/ket.jpg alt="Ket singlpage PSD template" />
+				<figcaption class="mask">
+					<h3>Ket Web template</h3>
+					<span>Tempaltes</span>
+				</figcaption>
+			</figure>
+			<figure class="mix portfolio-item templates col-md-3  col-sm-6">
+				<img class="img-responsive" src=http://kosmetologist.com/wp-content/themes/Kosmo/img/portfolio/mentum.png alt="Mentum web theme" />
+				<figcaption class="mask">
+					<h3>Mentum web theme</h3>
+					<span>Templates</span>
+				</figcaption>
+			</figure>
+			<figure class="mix portfolio-item web mockups col-md-3  col-sm-6">
+				<img class="img-responsive" src=http://kosmetologist.com/wp-content/themes/Kosmo/img/portfolio/modern-phone.jpg alt="Smartphone mockup" />
+
+				<figcaption class="mask">
+					<h3>Smartphone mockup</h3>
+					<span>Mockups</span>
+				</figcaption>
+			</figure>
+			<figure class="mix portfolio-item print ui-kits col-md-3  col-sm-6">
+				<img class="img-responsive" src=http://kosmetologist.com/wp-content/themes/Kosmo/img/portfolio/sven.jpg alt="Sven UI Kit" />
+				<figcaption class="mask">
+					<h3>Sven UI Kit</h3>
+					<span>UI Kits</span>
+				</figcaption>
+			</figure>
+
+		</div>
+	</div>
+</section>
+<!-- End portfolio section -->
+<!-- Start about section -->
+<section id="about">
+	<div class="baner">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 box">
+					<h3>Some words about us</h3>
+					<span class="delimiter"></span>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.
+					</p>
+					<a href="#" class="btn btn-primary">Read more</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<header>
+			<h2 class="wow fadeInDown">Meet our team</h2>
+			<p class="wow fadeInUp" data-wow-delay="0.1s">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</p>
+		</header>
+		<div class="row">
+			<div class="col-sm-4 wow fadeInUp">
+				<div class="team-member">
+					<div class="member-photo">
+						<img src=http://kosmetologist.com/wp-content/themes/Kosmo/img/team/team-3.jpg class="img-responsive" alt="Team member 1">
+						<div class="overlay">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing.
+							</p>
+							<ul class="social">
+								<li><a href=""><i class="fa fa-facebook"></i></a></li>
+								<li><a href=""><i class="fa fa-twitter"></i></a></li>
+								<li><a href=""><i class="fa fa-pinterest"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="basic-info">
+						<h4>John Doe</h4>
+						<span>Graphic designer</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4 wow fadeInUp" data-wow-delay="0.1s">
+				<div class="team-member">
+					<div class="member-photo">
+						<img src=http://kosmetologist.com/wp-content/themes/Kosmo/img/team/team-2.jpg class="img-responsive" alt="Team member 1">
+						<div class="overlay">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing.
+							</p>
+							<ul class="social">
+								<li><a href=""><i class="fa fa-facebook"></i></a></li>
+								<li><a href=""><i class="fa fa-twitter"></i></a></li>
+								<li><a href=""><i class="fa fa-pinterest"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="basic-info">
+						<h4>John Doe</h4>
+						<span>Graphic designer</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4 wow fadeInUp" data-wow-delay="0.2s">
+				<div class="team-member">
+					<div class="member-photo">
+						<img src=http://kosmetologist.com/wp-content/themes/Kosmo/img/team/team-1.jpg class="img-responsive" alt="Team member 3">
+						<div class="overlay">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing.
+							</p>
+							<ul class="social">
+								<li><a href=""><i class="fa fa-facebook"></i></a></li>
+								<li><a href=""><i class="fa fa-twitter"></i></a></li>
+								<li><a href=""><i class="fa fa-pinterest"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="basic-info">
+						<h4>John Doe</h4>
+						<span>Graphic designer</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- End about section -->
+<!-- Start blog section -->
+<section id="blog" class="dark">
+	<div class="container">
+		<header>
+			<h2 class="wow fadeInDown">Our Blog Stories</h2>
+			<p class="wow fadeInUp" data-wow-delay="0.1s">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut.
+			</p>
+		</header>
+	</div>
+	<div class="blog-items clearfix">
+		<div class="blog-item col-md-4 wow fadeInLeft">
+			<img src=http://kosmetologist.com/wp-content/themes/Kosmo/img/blog-1.png align="Blog post" class="img-responsive" alt="Blog post" />
+			<div class="meta-data">
+				<h3>Awesome blog title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco.
+				</p>
+				<a href="#">Read More...</a>
+			</div>
+		</div>
+		<div class="blog-item col-md-4 wow fadeInLeft" data-wow-delay="0.2s">
+			<img src=http://kosmetologist.com/wp-content/themes/Kosmo/img/blog-2.png align="Blog post" class="img-responsive" alt="Blog post" />
+			<div class="meta-data">
+				<h3>Awesome blog title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco.
+				</p>
+				<a href="#">Read More...</a>
+			</div>
+		</div>
+		<div class="blog-item col-md-4 wow fadeInLeft" data-wow-delay="0.4s">
+			<img src=http://kosmetologist.com/wp-content/themes/Kosmo/img/blog-3.png align="Blog post" class="img-responsive" alt="Blog post" />
+			<div class="meta-data">
+				<h3>Awesome blog title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation.
+				</p>
+				<a href="#">Read More...</a>
+			</div>
+		</div>
+	</div>
+	<div class="cleadfix">
+		<div class="text-center load-more">
+			<a href="#" class="fa fa-plus circled"></a>
+		</div>
+	</div>
+</section>
+<!-- End blog section -->
+<!-- Start  section -->
+<section id="contact">
+	<div class="container">
+		<header>
+			<h2 class="wow fadeInDown">Keep in touch</h2>
+			<p class="wow fadeInUp" data-wow-delay="0.1s">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut.
+			</p>
+		</header>
+		<form class="row">
+			<div class="form-group col-md-6">
+				<input name="name" type="text" placeholder="Name" class="form-control" />
+			</div>
+			<div class="form-group col-md-6">
+				<input name="mail" type="email" placeholder="Email" class="form-control" />
+			</div>
+			<div class="form-group col-md-12">
+				<input name="subject" type="subject" placeholder="Subject" class="form-control" />
+			</div>
+			<div class="form-group col-md-12">
+				<textarea name="message" class="form-control" rows="10" placeholder="Message"></textarea>
+			</div>
+			<div class="form-group col-md-12">
+				<button class="btn btn-lg btn-primary">Send</button>
+			</div>
+		</form>
+		<div class="address">
+			<div class="row">
+				<div class="col-md-4 text-center wow zoomIn">
+					<i class="fa fa-phone circled"></i>
+					<span>847-939-9359</span>
+				</div>
+				<div class="col-md-4 text-center wow zoomIn" data-wow-delay="0.2s">
+					<i class="fa fa-envelope circled"></i>
+					<span>sevenmail@sevenmail.com</span>
+				</div>
+				<div class="col-md-4 text-center wow zoomIn" data-wow-delay="0.4s">
+					<i class="fa fa-globe circled"></i>
+					<span>4116 Oak Avenue</span>
+					<span>Chickago, IL 60607</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer()?>
+<!-- End contact section -->
+<!-- Start footer section -->
