559e833c738bc2e6a0be9966beaa669c27306b09
baga94 <37371002+baga94@users.noreply.github.com>
2018-03-14 15:55:50 +0200

    add all files

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer>
+	<div class="container">
+		<p class="copyright">
+			&copy; 2015 All rights reserved - Theme by <a href="http://graphberry.com" target="_blank">
+			<b>GraphBerry</b></a>
+		</p>
+		<ul class="social">
+			<li class="wow bounceIn"><a href="http://goo.gl/RqhEjP" target="_blank"><i class="fa fa-facebook"></i></a></li>
+			<li class="wow bounceIn" data-wow-delay="0.1s"><a href="http://goo.gl/hUfpSB" target="_blank"><i class="fa fa-twitter"></i></a></li>
+			<li class="wow bounceIn" data-wow-delay="0.2s"><a href="http://goo.gl/k9zAy5" target="_blank"><i class="fa fa-dribbble"></i></a></li>
+			<li class="wow bounceIn" data-wow-delay="0.3s"><a href="http://goo.gl/FPjuCE" target="_blank"><i class="fa fa-behance"></i></a></li>
+			<li class="wow bounceIn" data-wow-delay="0.4s"><a href="http://goo.gl/UYjGTR" target="_blank"><i class="fa fa-pinterest"></i></a></li>
+			<li class="wow bounceIn" data-wow-delay="0.5s"><a href="http://goo.gl/r4xzR4" target="_blank"><i class="fa fa-google"></i></a></li>
+		</ul>
+		<a href="index.html#home" data-scroll class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+	</div>
+</footer>
+<!-- End footer section -->
+
+<!-- Load jQuery -->
+<script type="text/javascript" src=http://kosmetologist.com/wp-content/themes/Kosmo/js/jquery-1.11.2.min.js></script>
+
+<!-- Load Booststrap -->
+<script type="text/javascript" src=http://kosmetologist.com/wp-content/themes/Kosmo/js/bootstrap.js></script>
+
+<!-- Load custom js plugins -->
+<script type="text/javascript" src=http://kosmetologist.com/wp-content/themes/Kosmo/js/jquery.mixitup.js></script>
+<script type="text/javascript" src=http://kosmetologist.com/wp-content/themes/Kosmo/js/jquery.magnific-popup.min.js></script>
+<script type="text/javascript" src=http://kosmetologist.com/wp-content/themes/Kosmo/js/wow.js></script>
+<script type="text/javascript" src=http://kosmetologist.com/wp-content/themes/Kosmo/js/smooth-scroll.js></script>
+
+<!-- Load custom js for theme -->
+<script type="text/javascript" src=http://kosmetologist.com/wp-content/themes/Kosmo/js/app.js></script>
+<?php wp_footer();?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,63 @@
+<!--
+ * A Design by GraphBerry
+ * Author: GraphBerry
+ * Author URL: http://graphberry.com
+ * License: http://graphberry.com/pages/license
+-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php bloginfo('name ')?></title>
+
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Load fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
+
+	<!--Load styles -->
+	<link rel="stylesheet" type="text/css" href="http://kosmetologist.com/wp-content/themes/Kosmo/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="http://kosmetologist.com/wp-content/themes/Kosmo/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="http://kosmetologist.com/wp-content/themes/Kosmo/css/magnific-popup.css">
+	<link rel="stylesheet" type="text/css" href="http://kosmetologist.com/wp-content/themes/Kosmo/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>">
+    <?php wp_head();?>
+    
+</head>
+<body>
+
+	<header>
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#"><img src=<?php bloginfo('template_url')?>/img/logo.png alt="Seven HTML theme" /></a>
+				</div>
+				<div class="collapse navbar-collapse navbar-right" id="navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a data-scroll href="index.html#home">Главная</a></li>
+						<li><a data-scroll href="index.html#services">Услуги</a></li>
+						<li><a data-scroll href="index.html#portfolio">Наши работы</a></li>
+						<li><a data-scroll href="index.html#about">О нас</a></li>
+						<li><a data-scroll href="index.html#blog">Отзывы</a></li>
+						<li><a data-scroll href="index.html#contact">Контакты</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<div id="home" class="jumbotron slide slide-fullscreen">
+		<div class="container">
+			<span>Awesome theme</span>
+			<h1><?php bloginfo('name ')?></h1>
+		
+				</div>
+	</div>
